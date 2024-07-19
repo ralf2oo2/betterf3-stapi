8316c1f31a4beda02dc0308fc9d10b509f76b5c9
Ralf2oo2 <ralf2oo2@hotmail.com>
2024-07-19 19:27:45 +0200

    Formatted locationmodule update method

--- src/main/java/ralf2oo2/betterf3/modules/LocationModule.java
+++ src/main/java/ralf2oo2/betterf3/modules/LocationModule.java
@@ -47,34 +47,69 @@ public class LocationModule extends BaseModule{
     }
     @Override
     public void update(Minecraft minecraft) {
-        String dimensionId;
-        String biome = minecraft.world.method_1781().method_1787((int)Math.floor(minecraft.player.x), (int)Math.floor(minecraft.player.y)).name;
-        int lookDir = (MathHelper.floor((double)(minecraft.player.yaw * 4.0F / 360.0F) + 0.5) & 3);
-        //the output of this seems incorrect
-        int topY = minecraft.world.getTopY((int)Math.floor(minecraft.player.x), (int)Math.floor(minecraft.player.z));
+        String biome = minecraft.world.method_1781().method_1787(
+            (int) Math.floor(minecraft.player.x),
+            (int) Math.floor(minecraft.player.y)
+        ).name;
 
+        int lookDir = (MathHelper.floor(
+        (double) (minecraft.player.yaw * 4.0F / 360.0F) + 0.5) & 3
+        );
+
+        int topY = minecraft.world.getTopY(
+            (int) Math.floor(minecraft.player.x),
+            (int) Math.floor(minecraft.player.z)
+        );
+
+        String dimensionId;
         Optional dimensionContainer = DimensionRegistry.INSTANCE.getByLegacyId(minecraft.player.dimensionId);
         if(dimensionContainer.isEmpty()){
             dimensionId = "Invalid";
         } else {
             dimensionId = DimensionRegistry.INSTANCE.getId((DimensionContainer<?>) dimensionContainer.get()).toString();
         }
+
         lines.get(0).setValue(dimensionId);
-        lines.get(1).setValue(TranslationStorage.getInstance().get("text.betterf3.line." + directions[lookDir]) + " (" + TranslationStorage.getInstance().get("text.betterf3.line." + coord_directions[lookDir]) + ")");
+        lines.get(1).setValue(String.format("%s (%s)",
+            TranslationStorage.getInstance().get("text.betterf3.line." + directions[lookDir]),
+            TranslationStorage.getInstance().get("text.betterf3.line." + coord_directions[lookDir])
+        ));
+
         String yaw = String.format("%.1f", Utils.wrapDegrees(minecraft.player.yaw));
         String pitch = String.format("%.1f", Utils.wrapDegrees(minecraft.player.pitch));
         lines.get(2).setValue(String.format(TranslationStorage.getInstance().get("format.betterf3.rotation"), yaw, pitch));
 
-        Chunk chunk = minecraft.world.method_199((int)Math.floor(minecraft.player.x), (int)Math.floor(minecraft.player.z));
-        int blockLight = chunk.getLight(LightType.BLOCK, ((int)Math.floor(minecraft.player.x)) & 15, (int)Math.floor(minecraft.player.y), ((int)Math.floor(minecraft.player.z)) & 15);
-        int skylight = chunk.getLight(LightType.SKY, ((int)Math.floor(minecraft.player.x)) & 15, (int)Math.floor(minecraft.player.y), ((int)Math.floor(minecraft.player.z)) & 15);
-        //String.valueOf(minecraft.player.world.method_255((int)Math.floor(minecraft.player.x), (int)Math.floor(minecraft.player.y), (int)Math.floor(minecraft.player.z)))
-        //minecraft.world.field_202
-        lines.get(3).setValue(String.format(TranslationStorage.getInstance().get("format.betterf3.chunklight", Math.max(skylight, blockLight), skylight, blockLight)));
+        Chunk chunk = minecraft.world.method_199(
+            (int) Math.floor(minecraft.player.x),
+            (int) Math.floor(minecraft.player.z)
+        );
+
+        int blockLight = chunk.getLight(
+            LightType.BLOCK,
+            ((int) Math.floor(minecraft.player.x)) & 15,
+            (int) Math.floor(minecraft.player.y),
+            ((int) Math.floor(minecraft.player.z)) & 15
+        );
+
+        int skyLight = chunk.getLight(
+            LightType.SKY,
+            ((int) Math.floor(minecraft.player.x)) & 15,
+            (int) Math.floor(minecraft.player.y),
+            ((int) Math.floor(minecraft.player.z)) & 15
+        );
+
+        lines.get(3).setValue(String.format(
+                TranslationStorage.getInstance().get("format.betterf3.chunklight"),
+                Math.max(skyLight, blockLight), skyLight, blockLight
+        ));
+
         lines.get(4).setValue(String.valueOf(topY - 1));
+
         lines.get(5).setValue(biome);
+
         String difficulty = Utils.getDifficultyString(minecraft.world.field_213);
         lines.get(6).setValue(difficulty);
+
         lines.get(7).setValue(String.valueOf((int)Math.floor(minecraft.world.getTime() / 24000)));
     }
 }
