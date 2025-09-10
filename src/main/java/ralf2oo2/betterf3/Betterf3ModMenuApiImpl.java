3949ed1ea47d5b5f77d294aa45c82da3966ce3dd
Ralf2oo2 <ralf2oo2@hotmail.com>
2025-09-10 20:19:52 +0200

    Update to Mod Menu Babric

--- src/main/java/ralf2oo2/betterf3/Betterf3ModMenuApiImpl.java
+++ src/main/java/ralf2oo2/betterf3/Betterf3ModMenuApiImpl.java
@@ -1,6 +1,7 @@
 package ralf2oo2.betterf3;
 
-import io.github.prospector.modmenu.api.ModMenuApi;
+import net.danygames2014.modmenu.api.ConfigScreenFactory;
+import net.danygames2014.modmenu.api.ModMenuApi;
 import net.minecraft.client.gui.screen.Screen;
 import ralf2oo2.betterf3.config.gui.ModConfigScreen;
 
@@ -8,12 +9,7 @@ import java.util.function.Function;
 
 public class Betterf3ModMenuApiImpl implements ModMenuApi {
     @Override
-    public String getModId() {
-        return Betterf3.MODID.toString();
-    }
-
-    @Override
-    public Function<Screen, ? extends Screen> getConfigScreenFactory() {
-        return parent -> new ModConfigScreen();
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return (screen) -> new ModConfigScreen();
     }
 }
